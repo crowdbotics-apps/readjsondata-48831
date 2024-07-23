d63b59b2cd0d975cf487dd55510e6d6cef44be35
crowdbotics-apps <team@crowdbotics.com>
2024-07-23 18:15:51 +0000

    Update models

--- backend/readjsondata_48831/wsgi.py
+++ backend/readjsondata_48831/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "readjsondata_48831.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readjsondata_48831.settings")
 
 application = get_wsgi_application()
